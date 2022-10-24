d863cc58e06b60d09894b5c563b02ff0fc5edfcb
Brayan Leon <bdleons@unal.edu.co>
2022-10-24 02:04:25 -0500

    Finalmente Portale Version 1

--- sketchTest.js
+++ sketchTest.js
@@ -10,10 +10,14 @@ let fox;
 // texture
 let fox_tex;
 
-let modelPOS, modelDis;
-
-let fbo1, fbo2, fbo3;
-let cam1, cam2, cam3;
+let set1 = 0, set2 = 0;
+let starCheck1 = 0, starCheck2 = 0;
+let relativePosCon, relativePosSubCon, relativePosRotCon;
+let relativePrevPosCon, relativePrevPosSubCon, relativePrevPosRotCon;
+let teleportedFbo1 = false, teleportedFbo2 = true;
+let fbo1, fbo2, fbo1TextPort, fbo2TextPort;
+let cam1, cam2, cam1Off, cam2Off;
+let cam1Pos, cam2Pos, cam1OffPos, cam2OffPos;
 
 function preload() {
     kemonaSilver = loadImage('images/KemonaPlushSilver.jpg');
@@ -26,9 +30,8 @@ function setup() {
     // frame buffer object instances (FBOs)
     fbo1 = createGraphics(600, 600, WEBGL);
     fbo2 = createGraphics(600, 600, WEBGL);
-    fbo3 = createGraphics(600, 600, WEBGL);
-    fbo4 = createGraphics(100, 100, WEBGL);
-    fbo3.ortho(-fbo3.width / 2, fbo3.width / 2, -fbo3.height / 2, fbo3.height / 2, 1, 10000);
+    fbo1TextPort = createGraphics(600, 600, WEBGL);
+    fbo2TextPort = createGraphics(600, 600, WEBGL);
     // FBOs cams
     // cam1 for fbo1 // EasyCam P5
     cam1 = new Dw.EasyCam(fbo1._renderer);
@@ -55,9 +58,21 @@ function setup() {
     // cam3.state_reset = state3;   // state to use on reset (double-click/tap)
     // cam3.setViewport([1200, 0, 600, 600]);
 
-    // cam3 = fbo3.createCamera(); // LegacyCam P5
+    cam1Off = fbo1TextPort.createCamera(); // LegacyCam P5
+    cam2Off = fbo2TextPort.createCamera(); // LegacyCam P5
 
-    player = new Player(100, 0, 100);
+    playerOnfbo1 = new Player(100, 0, 100);
+    playerOnfbo2 = new Player(0, 0, 0);
+    playerOnfbo1Off = new Player(100, 0, 100);
+    playerOnfbo2Off = new Player(0, 0, 0);
+    portalOnfbo1 = new Portal(0, 0, -437, 0);
+    portalOnfbo2 = new Portal(0, 0, -437, 0);
+    portalOnfbo1.link(portalOnfbo2);
+
+    cam1OffPos = fbo1TextPort.treeLocation(/*[0, 0, 0],*/ { from: Tree.CAM, to: Tree.WORLD });
+    cam1Off.camera(cam1OffPos.x,cam1OffPos.y,-cam1OffPos.z, 0,0,0, 0,1,0)
+    cam2OffPos = fbo2TextPort.treeLocation(/*[0, 0, 0],*/ { from: Tree.CAM, to: Tree.WORLD });
+    cam2Off.camera(cam2OffPos.x,cam2OffPos.y,-cam2OffPos.z, 0,0,0, 0,1,0)
 
     document.oncontextmenu = function () { return false; }
     colorMode(RGB, 1);
@@ -65,17 +80,85 @@ function setup() {
 
 function draw() {
 
-    scene(fbo1, 1, 0);
-    scene(fbo2, 0, 0);
+    scene(fbo1TextPort, 1, 1, 0);
+    scene(fbo2TextPort, 0, 1, 0);
+    scene(fbo1, 1, 0, 1);
+    scene(fbo2, 0, 0, 1);
 
-    if (renderFbo1) {
-        player.movement();
-        player.render(fbo1);
+    if (!teleportedFbo1 && teleportedFbo2) {
+        if (set1) {
+            playerOnfbo1.pos = playerOnfbo2.playerPos;
+            playerOnfbo1.a = -playerOnfbo2.a;
+            set1 = 0
+        }
+        playerOnfbo1.movement();
+        playerOnfbo1.render(fbo1);
+        if (playerOnfbo1.playerPos != 0) {
+            playerOnfbo1Off.pos = playerOnfbo1.playerPos;
+            playerOnfbo1Off.a = playerOnfbo1.a;
+            playerOnfbo1Off.render(fbo1TextPort);
+        }
+        if (abs(playerOnfbo1.playerPos.z) < (abs(portalOnfbo1.portalPos.z) - 40)) {
+            if (renderFbo1) {
+                console.log("CheckActivado Portal 1");
+                renderFbo1 = 0;
+            }
+            starCheck1 = 1;
+        }
+        if (starCheck1) {
+            teleportedFbo1 = portalOnfbo1.check(playerOnfbo1)
+        }
+        if (teleportedFbo1) {
+            teleportedFbo2 = false;
+            starCheck1 = 0;
+            set2 = 1;
+        }
     }
+    if (!teleportedFbo2 && teleportedFbo1) {
+        if (set2) {
+            playerOnfbo2.pos = playerOnfbo1.playerPos;
+            playerOnfbo2.a = -playerOnfbo1.a;
+            set2 = 0
+        }
+        playerOnfbo2.movement();
+        playerOnfbo2.render(fbo2);
+        if (playerOnfbo2.playerPos != 0) {
+            playerOnfbo2Off.pos = playerOnfbo2.playerPos;
+            playerOnfbo2Off.a = playerOnfbo2.a;
+            playerOnfbo2Off.render(fbo2TextPort);
+        }
+        if (abs(playerOnfbo2.playerPos.z) < (abs(portalOnfbo2.portalPos.z) - 40)) {
+            if (renderFbo1) {
+                console.log("CheckActivado Portal 2");
+                renderFbo1 = 0;
+            }
+            starCheck2 = 1;
+        }
+        if (starCheck2) {
+            teleportedFbo2 = portalOnfbo2.check(playerOnfbo2)
+        }
+        if (teleportedFbo2) {
+            teleportedFbo1 = false;
+            starCheck2 = 0;
+            set1 = 1;
+        }
+    }
+    portalOnfbo1.render(fbo1, 0, fbo2TextPort);
+    portalOnfbo2.render(fbo2, 1, fbo1TextPort);
 
     if (press) {
-        console.log(modelPOS);
-        console.log(modelDis);
+        console.log("Accediste a la informacion de los Player1");
+        console.log(playerOnfbo1.playerDis);
+        // console.log("Accediste a la informacion del Portal1");
+        // console.log(portalOnfbo1.portalPos);
+        // console.log("Informacion de Check Player");
+        // console.log(relativePosCon);
+        // console.log(relativePosSubCon);
+        // console.log(relativePosRotCon);
+        // console.log("Informacion de Check Player Previa");
+        // console.log(relativePrevPosCon);
+        // console.log(relativePrevPosSubCon);
+        // console.log(relativePrevPosRotCon);
         press = 0;
     }
     angle += 0.007
@@ -93,6 +176,8 @@ class Player {
         this.a = 0;
         this.speed = 3;
         this.aspeed = PI / 90;
+        this.playerPos = 0;
+        this.playerDis = 0;
     }
 
     move(fw) {
@@ -114,8 +199,8 @@ class Player {
         if (this.pos.x < -400) {
             this.pos.x = -400
         }
-        if (this.pos.z < -400) {
-            this.pos.z = -400
+        if (this.pos.z < -410) {
+            this.pos.z = -410
         }
     }
 
@@ -148,13 +233,68 @@ class Player {
         fbo.pop();
         fbo.texture(fox_tex);
         fbo.model(fox);
-        modelPOS = fbo1.treeLocation(/*[0, 0, 0],*/ { from: Tree.MODEL, to: Tree.WORLD });
-        modelDis = fbo1.treeDisplacement(/*[0, 0, 0],*/ { from: Tree.MODEL, to: Tree.WORLD });
+        this.playerPos = fbo.treeLocation(/*[0, 0, 0],*/ { from: Tree.MODEL, to: Tree.WORLD });
+        this.playerDis = fbo.treeDisplacement(/*[0, 0, 0],*/ { from: Tree.MODEL, to: Tree.WORLD });
         fbo.pop();
         fbo.pop();
     }
 }
-function scene(fbo, centerObject, frontWall) {
+class Portal {
+    constructor(x, y, z, a) {
+        this.pos = createVector(x, y, z);
+        this.len = 220;
+        this.a = a;
+        this.portalPos = 0;
+        this.linkedPortal = null;
+    }
+
+    link(other) {
+        this.linkedPortal = other;
+        other.linkedPortal = this;
+    }
+
+    check_(player) {
+        const relativePos = player.playerPos.copy();
+        relativePosCon = relativePos;
+        relativePosSubCon = relativePos.sub(this.portalPos);
+        relativePosRotCon = relativePos.rotate(-this.a);
+        const relativePrevPos = player.prevPos.copy();
+        relativePrevPosCon = relativePrevPos;
+        relativePrevPosSubCon = relativePrevPos.sub(this.portalPos);
+        relativePrevPosRotCon = relativePrevPos.rotate(-this.a);
+        if ((relativePos.z < 28) !== (relativePrevPos.z < 28) && abs(relativePos.x) < this.len / 2) {
+            console.log("Teletransportado");
+            player.a += this.linkedPortal.a - this.a;
+            return true;
+        }
+        return false;
+    }
+
+    check(player) {
+        return this.check_(player);
+    }
+
+    render(fbo, numberPortal, texElement) {
+        fbo.push()
+        fbo.translate(this.pos.x, this.pos.y, this.pos.z);
+        fbo.push()
+        fbo.translate(this.pos.x, this.pos.y, -1);
+        if (numberPortal === 0) {
+            fbo.fill(0, 0, 255)
+        } else {
+            fbo.fill(255, 158, 0)
+        }
+        fbo.circle(this.pos.x, this.pos.y, 260)
+        fbo.pop()
+        if (texElement) {
+            fbo.texture(texElement);
+        }
+        fbo.circle(this.pos.x, this.pos.y, 250)
+        this.portalPos = fbo.treeLocation(/*[0, 0, 0],*/ { from: Tree.MODEL, to: Tree.WORLD });
+        fbo.pop()
+    }
+}
+function scene(fbo, centerObject, frontWall, backWall) {
     fbo.background(120);
     fbo.reset();
     fbo.rectMode(CENTER);
@@ -164,10 +304,10 @@ function scene(fbo, centerObject, frontWall) {
     fbo.rotateZ(angle)
     fbo.rotateX(angle)
     fbo.rotateY(angle)
-    if(centerObject){
+    if (centerObject) {
         fbo.texture(kemonaSilver)
         fbo.box(100)
-    }else{
+    } else {
         fbo.texture(kemonaRed)
         fbo.box(100)
     }
@@ -192,14 +332,16 @@ function scene(fbo, centerObject, frontWall) {
         fbo.box(900, 250, 20);
         fbo.pop();
     }
-    // Trasera Z-
-    fbo.push();
-    fbo.stroke(2);
-    fbo.ambientLight(255);
-    fbo.ambientMaterial(100);
-    fbo.translate(0, -25, -450);
-    fbo.box(900, 250, 20);
-    fbo.pop();
+    if (backWall) {
+        // Trasera Z-
+        fbo.push();
+        fbo.stroke(2);
+        fbo.ambientLight(255);
+        fbo.ambientMaterial(100);
+        fbo.translate(0, -25, -450);
+        fbo.box(900, 250, 20);
+        fbo.pop();
+    }
     // Lateral Derecha X+
     fbo.push();
     fbo.stroke(2);
@@ -219,17 +361,17 @@ function scene(fbo, centerObject, frontWall) {
     fbo.box(900, 250, 20);
     fbo.pop();
 }
-function mousePressed() {
-    if (press === 0) {
-        press = 1;
-    }
-}
 function keyPressed() {
     if (keyCode === 80) {
         if (renderFbo1 === 0) {
             renderFbo1 = 1
         } else {
             renderFbo1 = 0
+        }
+    }
+    if (keyCode === 73) {
+        if (press === 0) {
+            press = 1;
         }
     }
 }
